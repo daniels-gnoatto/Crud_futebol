898f29950a7dd9d30f59d27c191002b6c8995c97
Daniels-gnoatto <daniel_gnoatto@estudante.sesisenai.org.br>
2025-08-22 08:57:44 -0300

    add - Coloquei botões na página do index para serem acessadas

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../style/style.css">
+</head>
+<body>
+
+    <a href="/jogadores/create.php">
+             <button class="botaoMenu">
+            <div>
+                <h4>Jogadores</h4>
+            </div>
+        </button>
+    </a>
+
+    <a href="/partidas/create.php">
+             <button class="botaoMenu">
+            <div>
+                <h4>Partidas</h4>
+            </div>
+        </button>
+    </a>
+
+    <a href="/times/create.php">
+             <button class="botaoMenu">
+            <div>
+                <h4>Times</h4>
+            </div>
+        </button>
+    </a>
+    
+</body>
+</html>
