e1c0426d98451ca0c50a30bd0a675d2506b086a0
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-08-22 09:29:40 -0300

    add arrumei o index e o css

--- public/index.php
+++ public/index.php
@@ -1,35 +1,31 @@
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../../style/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Menu</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
+  <div class="menu-container">
     <a href="jogadores/create.php">
-             <button class="botaoMenu">
-            <div>
-                <h4>Jogadores</h4>
-            </div>
-        </button>
+      <button class="botaoMenu">
+        <h4>Jogadores</h4>
+      </button>
     </a>
 
     <a href="partidas/create.php">
-             <button class="botaoMenu">
-            <div>
-                <h4>Partidas</h4>
-            </div>
-        </button>
+      <button class="botaoMenu">
+        <h4>Partidas</h4>
+      </button>
     </a>
 
     <a href="times/create.php">
-             <button class="botaoMenu">
-            <div>
-                <h4>Times</h4>
-            </div>
-        </button>
+      <button class="botaoMenu">
+        <h4>Times</h4>
+      </button>
     </a>
-    
+  </div>
+
 </body>
 </html>
